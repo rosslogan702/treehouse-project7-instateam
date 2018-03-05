f50afae69a7d2abad6e75c3f3e772d37aa80b6f2
rosslogan702 <rosslogan702@gmail.com>
2018-03-05 20:58:50 +0000

    First working use case of end to end in the system, roles can now be written to the database after being entered on the UI

--- src/main/java/com/rosslogan/instateam/model/Role.java
+++ src/main/java/com/rosslogan/instateam/model/Role.java
@@ -14,7 +14,6 @@ public class Role {
     private Long id;
 
     @NotNull
-    @Pattern(regexp = "[a-zA-Z0-9]+")
     private String name;
 
     public Role() {
--- src/main/java/com/rosslogan/instateam/web/controller/RoleController.java
+++ src/main/java/com/rosslogan/instateam/web/controller/RoleController.java
@@ -5,8 +5,12 @@ import com.rosslogan.instateam.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -22,6 +26,16 @@ public class RoleController {
         List<Role> roles = roleService.findAll();
 
         model.addAttribute("roles",roles);
+        //Blank role attribute for adding a new role
+        model.addAttribute("role", new Role());
         return "roles";
+    }
+
+
+    // Add a category
+    @RequestMapping(value = "/roles", method = RequestMethod.POST)
+    public String addCategory(@Valid Role role) {
+        roleService.save(role);
+        return "redirect:/roles";
     }
 }

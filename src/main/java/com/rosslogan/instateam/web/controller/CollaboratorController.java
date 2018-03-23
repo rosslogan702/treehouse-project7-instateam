f253bfdefa9323b5ec948f6bd00f2ba168a34c2b
rosslogan702 <rosslogan702@gmail.com>
2018-03-23 23:36:03 +0000

    Added the ability to add collaborators to the Collaborators tab

--- src/main/java/com/rosslogan/instateam/web/controller/CollaboratorController.java
+++ src/main/java/com/rosslogan/instateam/web/controller/CollaboratorController.java
@@ -7,8 +7,12 @@ import com.rosslogan.instateam.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -25,11 +29,23 @@ public class CollaboratorController {
         if(!model.containsAttribute("collaborator")) {
             model.addAttribute("collaborator", new Collaborator());
         }
-        // TODO: Get all collaborators and roles
+
         List<Collaborator> collaborators = collaboratorService.findAll();
         List<Role>  roles = roleService.findAll();
         model.addAttribute("collaborators",collaborators);
         model.addAttribute("roles", roles);
         return "collaborators";
+    }
+
+    // Add a category
+    @RequestMapping(value = "/collaborators", method = RequestMethod.POST)
+    public String addCategory(@Valid Collaborator collaborator, BindingResult result, RedirectAttributes redirectAttributes) {
+        if(result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.collaborator",result);
+            redirectAttributes.addFlashAttribute("collaborator", collaborator);
+            return "redirect:/collaborators";
+        }
+        collaboratorService.save(collaborator);
+        return "redirect:/collaborators";
     }
 }

0fee0d531b49e09658b715c4aa7d8afa7b2faa76
rosslogan702 <rosslogan702@gmail.com>
2018-04-01 20:17:35 +0100

    Added feature to not allow null role to be added for collaborator in database

--- src/main/java/com/rosslogan/instateam/model/Collaborator.java
+++ src/main/java/com/rosslogan/instateam/model/Collaborator.java
@@ -13,6 +13,7 @@ public class Collaborator {
     @Pattern(regexp = "[a-zA-Z -]+", message = "Invalid character(s) entered")
     private String name;
     @ManyToOne
+    @NotNull
     private Role role;
 
     public Collaborator() {

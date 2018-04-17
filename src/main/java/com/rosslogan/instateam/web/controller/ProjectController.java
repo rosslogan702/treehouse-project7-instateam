04ecc9d3ae2bbbbb3e7d2ff710832ba8b47850b7
rosslogan702 <rosslogan702@gmail.com>
2018-04-17 22:05:32 +0100

    Fixed new project index page to include the roles from the database table now rather than pre-populated HTML

--- src/main/java/com/rosslogan/instateam/web/controller/ProjectController.java
+++ src/main/java/com/rosslogan/instateam/web/controller/ProjectController.java
@@ -1,11 +1,19 @@
 package com.rosslogan.instateam.web.controller;
 
+import com.rosslogan.instateam.model.Collaborator;
+import com.rosslogan.instateam.model.Role;
+import com.rosslogan.instateam.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class ProjectController {
+    @Autowired
+    RoleService roleService;
 
     @RequestMapping(value = {"/projects", "/"})
     public String listProjects(Model model) {
@@ -14,6 +22,9 @@ public class ProjectController {
 
     @RequestMapping("/projects/add")
     public String addProject(Model model){
+        List<Role>  roles = roleService.findAll();
+        model.addAttribute("roles", roles);
+
         return "edit_project";
     }
 
